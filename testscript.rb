ee66aa5fdb36dbf08938bed615974dab174ee982
kapil-huma <kapil@huma.ai>
2022-03-24 17:10:43 +0530

    fjg

---
+++ testscript.rb
@@ -0,0 +1,14 @@
+
+class Testscript < Formula
+  desc ""
+  homepage ""
+  url "https://github.com/kapil-huma/testscript/archive/refs/tags/v1.0.0.tar.gz"
+  sha256 "ac7ca89c4adb48110bfb303e066a6421eacf7b64df9aae3695d83115753b4ed9"
+  license ""
+
+
+  def install
+	bin.install "testscript"
+  end
+
+end
